8026a210bb8bf8d12afd32921e3f2b2369572517
rnferrer <rnferrer@ucdavis.edu>
2024-12-12 16:30:49 -0800

    Created route and controller structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from "express"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import cors from "cors"
-
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+/* ROUTE IMPORTS */
+import userRoutes from "./routes/userRouter";
+import appliedJobRoutes from "./routes/appliedJobRouter";
+import savedJobRoutes from "./routes/savedJobRouter";
+import interviewRoutes from "./routes/interviewRouter";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -16,6 +20,11 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
+
+app.use("/user", userRoutes);
+app.use("/applied", appliedJobRoutes);
+app.use("/saved", savedJobRoutes);
+app.use("/interview", interviewRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3001;
